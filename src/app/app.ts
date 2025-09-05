0dbae6c7d47678dec739f1cd6e7868924a92c5ea
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2025-09-06 00:07:04 +0530

    removed unused import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, computed, inject } from '@angular/core';
 import { Header } from "./components/header/header";
 import { AppContainer } from "./components/app-container/app-container";
-import { Login } from "./components/login/login";
 @Component({
   selector: 'app-root',
-  imports: [Header, AppContainer, Login],
+  imports: [Header, AppContainer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
